a3c7bc200eb1b3513656ff013be3ece692b20c6a
sbng <sbng@cisco.com>
2022-03-27 23:07:09 +0800

    change mouse.py

--- mouse.py
+++ mouse.py
@@ -3,15 +3,13 @@ from pynput import mouse
 import os
 import subprocess
 
-flock = ['flock','-n','-u','-s','/tmp/cmd.lock']
 cmd1 = '/home/sbng/.config/rofi/applets/applets/time.sh'
 cmd2 = 'rofi -show window -window-format {w}{t} -theme /home/sbng/.config/rofi/launchers/misc/launchpad.rasi -pid /tmp/rofi.pid'
 
 def on_move(x, y):
-  if (x == 0):
-    process = subprocess.Popen(cmd1.split(), stderr=subprocess.DEVNULL)
-  if (y == 0):
-    process = subprocess.Popen(cmd2.split(), stderr=subprocess.DEVNULL) 
+  if (x == 0) and (y == 0):
+    p1 = subprocess.Popen(cmd1.split(), stderr=subprocess.DEVNULL)
+    p2 = subprocess.Popen(cmd2.split(), stderr=subprocess.DEVNULL) 
 
 with mouse.Listener( on_move=on_move) as listener:
     try:
