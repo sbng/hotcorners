7ace8c746428d962939b1988e121119c76cbf0cf
sbng <sbng@cisco.com>
2022-03-27 22:47:11 +0800

    clean up mouse.py

--- mouse.py
+++ mouse.py
@@ -7,17 +7,12 @@ flock = ['flock','-n','-u','-s','/tmp/cmd.lock']
 cmd1 = '/home/sbng/.config/rofi/applets/applets/time.sh'
 cmd2 = 'rofi -show window -window-format {w}{t} -theme /home/sbng/.config/rofi/launchers/misc/launchpad.rasi -pid /tmp/rofi.pid'
 
-if os.path.exists("/tmp/cmd.lock"):
-  os.remove("/tmp/cmd.lock")
-
 def on_move(x, y):
   if (x == 0):
     process = subprocess.Popen(cmd1.split(), stderr=subprocess.DEVNULL)
   if (y == 0):
     process = subprocess.Popen(cmd2.split(), stderr=subprocess.DEVNULL) 
-#     if (x != 0) and (y != 0):
-#       if os.path.exists("/tmp/rofi.lock"):
-#         print ("No rofi")
+
 with mouse.Listener( on_move=on_move) as listener:
     try:
         listener.join()
